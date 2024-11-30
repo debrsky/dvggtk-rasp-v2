1c7448301349c4f4dd497e83fb681c19f195f29e
debrsky <debrsky@gmail.com>
2024-12-01 03:17:22 +1000

    force new service worker activation and cache cleanup

--- www/service-worker.js
+++ www/service-worker.js
@@ -17,6 +17,24 @@ self.addEventListener('install', event => {
       ]);
     })
   );
+
+  // Принудительно активировать новый сервис-воркер
+  // self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+          return undefined;
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
