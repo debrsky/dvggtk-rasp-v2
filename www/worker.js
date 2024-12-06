88a3230eb7faef8bb30033aea4169a26df76db6d
debrsky <debrsky@gmail.com>
2024-12-06 18:38:59 +1000

    refactor worker.js to streamline client handling and updater checks

--- www/worker.js
+++ www/worker.js
@@ -3,64 +3,55 @@ importScripts('./db.js', './updater.js');
 console.log('[worker.js] starting');
 
 const SYNC_INTERVAL = 15_000;
+const clientPorts = new Set();
 
-const clientPorts = [];
 if (self.onconnect !== undefined) {
+  console.log('[worker.js] working as Shared Worker');
   self.onconnect = function (event) {
     console.log('[worker.js] self.onconnect starting');
-    const port = event.ports[0] // В ports всегда один элемент
-    clientPorts.push(port);
+    const port = event.ports[0];
+    clientPorts.add(port);
 
     port.onclose = function () {
-      const idx = clientPorts.indexOf(port);
-      if (idx === -1) return;
-      clientPorts.splice(idx, 1);
+      clientPorts.delete(port);
+      console.log('[worker.js] clientPort closed and removed from Set');
     };
+
+    if (self.DB_UPDATER) self.DB_UPDATER.syncWithServer();
   };
+} else {
+  console.log('[worker.js] working as Web Worker');
 }
 
-if (!self.DB_UPDATER) {
-  let counter = 0;
-  const timerId = setInterval(() => {
-    counter++;
-    if (self.DB_UPDATER) {
-      clearInterval(timerId);
+const startUpdater = () => {
+  const sendUpdateMessage = (err, status) => {
+    if (err) console.error(err);
+    const message = {
+      type: 'UPDATER',
+      payload: { status }
+    };
 
-      if (self.onconnect !== undefined) {
-        // Shared Worker
-        console.log('[worker.js] working as shared worker');
-        const sendUpdateMessageFromSharedWorker = async (err, status) => {
-          console.log('[worker.js] sendUpdateMessageFromSharedWorker starting');
-          if (err) console.error(err);
-
-          console.log('[worker.js] clientPorts', clientPorts);
-
-          clientPorts.forEach((clientPort) => clientPort.postMessage({
-            type: 'UPDATER',
-            payload: { status }
-          }));
-        };
-
-        self.DB_UPDATER.addUpdateListener(sendUpdateMessageFromSharedWorker);
-        self.DB_UPDATER.startSyncWithServer(SYNC_INTERVAL);
-      } else {
-        // Web Worker
-        console.log('[worker.js] working as web worker');
-        const sendUpdateMessageFromWebWorker = async (err, status) => {
-          if (err) console.error(err);
-
-          self.postMessage({
-            type: 'UPDATER',
-            payload: { status }
-          });
-        };
-
-        self.DB_UPDATER.addUpdateListener(sendUpdateMessageFromWebWorker);
-        self.DB_UPDATER.startSyncWithServer(SYNC_INTERVAL);
-      }
-      console.log(`[worker.js] try #${counter}: updater loaded`);
+    if (clientPorts.size > 0) {
+      clientPorts.forEach(clientPort => clientPort.postMessage(message));
     } else {
-      console.log(`[worker.js] try #${counter}: updater still not loaded`);
+      self.postMessage(message);
     }
-  }, 10);
+  };
+
+  self.DB_UPDATER.addUpdateListener(sendUpdateMessage);
+  self.DB_UPDATER.startSyncWithServer(SYNC_INTERVAL);
 };
+
+const checkUpdaterAvailability = async () => {
+  let counter = 0;
+  while (!self.DB_UPDATER) {
+    console.log(`[worker.js] try #${counter + 1}: updater still not loaded`);
+    counter++;
+    await new Promise(resolve => setTimeout(resolve, 10));
+  }
+
+  console.log(`[worker.js] try #${counter + 1}: updater loaded`);
+  startUpdater();
+};
+
+checkUpdaterAvailability();
